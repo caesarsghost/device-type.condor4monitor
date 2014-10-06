119a4c686a0f71d1bedd23be7a9f0f20c84a4c4c
caesarsghost <dhj.notify@gmail.com>
2014-10-05 20:39:52 -0500

    Multi-Zone
    Added support for 6 current zones

--- SmartApp.CondorStatusParent.groovy
+++ SmartApp.CondorStatusParent.groovy
@@ -26,6 +26,11 @@ definition(
 preferences {
 	section("Zones") {
 		input "zone1", "device.virtualContact", title:"Zone 1"
+        input "zone2", "device.virtualContact", title:"Zone 2"
+        input "zone3", "device.virtualContact", title:"Zone 3"
+        input "zone4", "device.virtualContact", title:"Zone 4"
+        input "zone5", "device.virtualContact", title:"Zone 5"
+        input "zone6", "device.virtualContact", title:"Zone 6"
 	}
 	section("Which Arduino Is your Condor Interface?") {
 		input "condor", "device.condorShield"
@@ -46,18 +51,62 @@ def updated() {
 }
 
 def initialize() {
-	subscribe( condor, "zone1.open", zone1Open )
-    subscribe( condor, "zone1.closed", zone1Close )
+
+	for(int i=1; i<=6; i++ )
+    {
+		subscribe( condor, "zone${i}.open", zoneOpen )
+    	subscribe( condor, "zone${i}.closed", zoneClose )
+    }
 }
 
-def zone1Open(evt)
+def zoneOpen(evt)
 {
-	log.debug "Setting Device Open"
-	zone1.open( "Zone 1" )
+	log.debug "Setting Device Open"   
+    switch( evt.name )
+    {
+        case "zone1":
+        zone1.open( "Zone 1" )
+        break;
+        case "zone2":
+        zone2.open( "Zone 2" )
+        break;
+        case "zone3":
+        zone3.open( "Zone 3" )
+        break;
+        case "zone4":
+        zone4.open( "Zone 4" )
+        break;
+        case "zone5":
+        zone5.open( "Zone 5" )
+        break;
+        case "zone6":
+        zone6.open( "Zone 6" )
+        break;        
+    };
 }
 
-def zone1Close(evt)
+def zoneClose(evt)
 {
 	log.debug "Setting Device Closed"
-	zone1.closed( "Zone 1" )
+    switch( evt.name )
+    {
+        case "zone1":
+        zone1.closed( "Zone 1" )
+        break;
+        case "zone2":
+        zone2.closed( "Zone 2" )
+        break;
+        case "zone3":
+        zone3.closed( "Zone 3" )
+        break;
+        case "zone4":
+        zone4.closed( "Zone 4" )
+        break;
+        case "zone5":
+        zone5.closed( "Zone 5" )
+        break;
+        case "zone6":
+        zone6.closed( "Zone 6" )
+        break;        
+    };
 }
